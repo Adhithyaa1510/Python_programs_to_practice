765e7fbc9767f66df793d961c094bb768c66efd2
Adhithyaa1510 <aathimusic@gmail.com>
2023-03-01 21:24:46 +0530

    first commit

---
+++ add.py
@@ -0,0 +1,4 @@
+number1=10
+number2=20
+sum=number1+number2
+print("addition of {0} and {1} is {2}" .format(number1,number2,sum))
---
+++ addition_of_two_runtime_numbers.py
@@ -0,0 +1,4 @@
+a=input("Enter your first value: ")
+b=input("Enter your second value: ")
+c=int(a)+int(b);
+print(c);
