5d31bb178d02d885b262f3fd5999e90bb85e11f3
Adhithyaa1510 <aathimusic@gmail.com>
2023-03-01 22:09:57 +0530

    commit on 01.03.2023

---
+++ creating_a_simple _calculator.py
@@ -0,0 +1,12 @@
+a=input("enter first value ")
+b=input("enter second value ")
+addition=int(a)+int(b)
+print("addition is {0}" .format(addition));
+subtraction=int(a)-int(b)
+print("subtraction is {0}" .format(subtraction));
+multiplication=int(a)*int(b)
+print("multiplication is {0}" .format(multiplication));
+division=int(a)/int(b)
+print("division is {0}" .format(division));
+modulus=int(a)%int(b)
+print("modulus is {0}" .format(modulus));
---
+++ greater_than_three_numbers.py
@@ -0,0 +1,10 @@
+a=input("enter first number ")
+b=input("enter second number ")
+c=input("enter third number ")
+if a>b:
+    if a>c:
+        print("First one is greater")
+elif b>c:
+        print("Second one is greater")
+else:
+    print("Third one is greater")
