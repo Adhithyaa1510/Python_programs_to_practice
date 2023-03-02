e66311449a361711b9c2cfa384513f8ef77b6fb7
Adhithyaa1510 <aathimusic@gmail.com>
2023-03-02 09:51:28 +0530

    commit on 02.03.2023

---
+++ first_and_last_digit_addition_of_a_given_number.py
@@ -0,0 +1,13 @@
+number=input("enter a number ")
+res = [int(x) for x in str(number)]
+print("convert list is" + str(res))
+sum=int(str(res[0]))+int(str(res[-1]))
+print(sum)
+
+    
+        
+        
+    
+ 
+
+
---
+++ getting_input_&_storing_in_List.py
@@ -0,0 +1,4 @@
+number=input("enter the input ")
+list1=[]
+list1[:0] = str(number)
+print(list1)
